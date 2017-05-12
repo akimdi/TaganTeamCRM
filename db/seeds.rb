9bface915d075e2c42a96e116d820acdc762c9d7
Akim <akimdi@gmail.com>
2017-05-12 14:01:51 +0300

    stact

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(email: 'test@test.com', password: '12345678', password_confirmation: '12345678')
+# AdminUser.create!(email: 'admin@admin.com', password: '12345678', password_confirmation: '12345678')
